6b4d9bf5d58d503760138c3cfb06bc42478c578f
JerekCar <78722478+JerekCar@users.noreply.github.com>
2024-03-13 23:53:33 -0600

    Update standard_training_home_page.dart
    Updated version of the home page with AutoZone colors and larger buttons

--- lib/standard_training_home_page.dart
+++ lib/standard_training_home_page.dart
@@ -1,14 +1,18 @@
+// Import core Flutter and Dart packages.
 import 'package:flutter/material.dart';
-import 'package:training_app/Basic_Employee/time_off.dart';
-import 'package:training_app/Basic_Employee/warranty_swap.dart';
-import 'package:training_app/web_view.dart'; // Assuming this is correctly pointing to your WebViewPage
-import 'package:training_app/Basic_Employee/znet_home.dart';
-import 'Basic_Employee/change_bat.dart'; // Assuming this is correctly pointing to your BatteryTestingGuidePage
-import 'Basic_Employee/checkout.dart';
-import 'Basic_Employee/wittdtjr.dart'; // Assuming this is correctly pointing to your WittdtjrPage
-import 'package:training_app/Basic_Employee/test_bat.dart'; // Assuming this is your custom page, ensure it's correctly imported
-import 'Basic_Employee/replace_wiper_blades.dart'; // Assuming this is correctly pointing to your ChangeWiperBladePage
 
+// Import local application pages and widgets.
+import 'package:training_app/time_off.dart';
+import 'package:training_app/warranty_swap.dart';
+import 'package:training_app/web_view.dart';
+import 'package:training_app/znet_home.dart';
+import 'change_bat.dart';
+import 'checkout.dart';
+import 'wittdtjr.dart';
+import 'test_bat.dart';
+import 'replace_wiper_blades.dart';
+
+// Define the main TrainingHomePage widget as a StatefulWidget.
 class TrainingHomePage extends StatefulWidget {
   const TrainingHomePage({Key? key}) : super(key: key);
 
@@ -16,165 +20,171 @@ class TrainingHomePage extends StatefulWidget {
   _TrainingHomePageState createState() => _TrainingHomePageState();
 }
 
+// Define the corresponding State class for TrainingHomePage.
 class _TrainingHomePageState extends State<TrainingHomePage> {
+  // Define the data for the buttons to be displayed in the grid.
   final List<Map<String, dynamic>> buttonData = [
     {'icon': Icons.battery_std, 'text': 'Change a Battery'},
-    {'icon': Icons.sell, 'text': 'WITTDTJR'},
-    {'icon': Icons.clear, 'text': 'Replace Wiper Blades'},
-    {'icon': Icons.alarm, 'text': 'Testing a Battery'},
-    {'icon': Icons.phone, 'text': 'Warranty Swap'},
-    {'icon': Icons.camera_alt, 'text': 'Checkout'},
-    {'icon': Icons.favorite, 'text': 'Time Off'},
-    {'icon': Icons.email, 'text': 'ZNET'},
-    {'icon': Icons.map, 'text': 'Website'},
+    {'icon': Icons.person, 'text': 'WITTDTJR'},
+    {'icon': Icons.swap_horiz, 'text': 'Replace Wiper Blades'},
+    {'icon': Icons.electric_bolt, 'text': 'Testing a Battery'},
+    {'icon': Icons.swap_vert_circle, 'text': 'Warranty Swap'},
+    {'icon': Icons.shopping_cart, 'text': 'Checkout'},
+    {'icon': Icons.calendar_month, 'text': 'Time Off'},
+    {'icon': Icons.sell, 'text': 'ZNET'},
+    {'icon': Icons.web, 'text': 'Website'},
   ];
 
   @override
   Widget build(BuildContext context) {
+    // Define the UI structure of the TrainingHomePage.
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Training App'),
-        toolbarHeight: 180.0, // Increased height
-        leading: null
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-              'Select an option below to get started:',
-              style: Theme.of(context).textTheme.titleLarge,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(10.0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3, // 3 items per row
-                crossAxisSpacing: 10.0, // Horizontal space between items
-                mainAxisSpacing: 10.0, // Vertical space between items
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(0.0),
+        child: Column(
+          children: [
+            // Header section.
+            Container(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0, top: 30.0),
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Color(0xFFF27405),
               ),
-              itemCount: buttonData.length, // Total number of items
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  elevation: 2.0, // Add elevation for shadow
-                  child: InkWell(
-                    onTap: () => _onButtonTap(context, index),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min, // Use minimum space
-                      children: <Widget>[
-                        Expanded(
-                          child: Icon(
-                            buttonData[index]['icon'],
-                            size: 50, // Icon size
-                          ),
-                        ),
-                        Text(
-                          buttonData[index]['text'],
-                          // Text description below the icon
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
+              child: const Text(
+                'Training App',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
+              ),
+            ),
+            // Content section.
+            Expanded(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'Select an option below to get started:',
+                      style: TextStyle(
+                        fontFamily: 'OpenSans',
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                );
-              },
+                  Expanded(
+                    child: _buildButtonGrid(),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
+  // Helper method to build the grid of buttons.
+  Widget _buildButtonGrid() {
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+      ),
+      itemCount: buttonData.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _buildGridItem(context, index);
+      },
+    );
+  }
+
+  // Helper method to build individual grid items.
+  Widget _buildGridItem(BuildContext context, int index) {
+    return Card(
+      elevation: 2.0,
+      color: const Color(0xFFF0F4FE),
+      child: InkWell(
+        onTap: () => _onButtonTap(context, index),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Expanded(
+              child: Icon(
+                buttonData[index]['icon'],
+                size: 80,
+                color: const Color(0xFFF2620F),
+              ),
+            ),
+            Text(
+              buttonData[index]['text'],
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontFamily: 'OpenSans',
+                color: Color(0xFF2b2b2b),
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Handle button taps and navigate to the appropriate page.
   void _onButtonTap(BuildContext context, int index) {
+    Widget page;
     switch (index) {
       case 0:
-        // Change a Battery
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => DetailPage(
-                    buttonText: 'Change a Battery',
-                  )),
-        );
+        page = DetailPage(buttonText: "Change a Battery");
         break;
       case 1:
-        // WITTDTJR
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const WittdtjrPage()),
-        );
+        page = const WittdtjrPage();
         break;
       case 2:
-        // Replace Wiper Blades
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => ChangeWiperBladePage(
-                    buttonText: 'Change Wiper Blade',
-                  )), // Ensure this is the correct constructor
-        );
+        page = ChangeWiperBladePage(buttonText: 'Replace Wiper Blades');
         break;
       case 3:
-        // Testing a Battery
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) =>
-                  const BatteryTestingGuidePage()), // Ensure this is the correct constructor
-        );
+        page = const BatteryTestingGuidePage();
         break;
       case 4:
-        // MISSING
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => WarrantySwapPage(
-                    buttonText: 'Warranty Swap',
-                  )), // Ensure this is the correct constructor
-        );
+        page = WarrantySwapPage(buttonText: 'Warranty Swap');
         break;
       case 5:
-        // MISSING
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => CheckoutPage(
-                    buttonText: 'Checkout',
-                  )), // Ensure this is the correct constructor
-        );
+        page = CheckoutPage(buttonText: 'Checkout');
         break;
       case 6:
-        // MISSING
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => TimeOffPage(
-                    buttonText: 'Time Off',
-                  )), // Ensure this is the correct constructor
-        );
+        page = TimeOffPage(buttonText: 'Time Off');
         break;
       case 7:
-        // MISSING
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) =>
-                  ZnetHome()), // Ensure this is the correct constructor
-        );
+        page = ZnetHome();
         break;
       case 8:
-        // Website
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => const WebViewPage(buttonText: 'Web View')),
-        );
+        page = const WebViewPage(buttonText: 'Website');
         break;
       default:
-        // Handle other cases or default navigation
-        break;
+        return; // Handle default or unknown case.
     }
+
+    // Navigate to the selected page with a fade transition.
+    Navigator.push(context, PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      },
+      transitionDuration: const Duration(milliseconds: 500),
+    ));
   }
 }
